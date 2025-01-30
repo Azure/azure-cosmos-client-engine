a50adc0a7e1d40227be2e7639e62ef6dfcf3c31b
Ashley Stanton-Nurse <github@analogrelay.net>
2025-01-30 22:58:06 +0000

    use query result comparer in merge strategy

--- engine/src/query/merge_strategy.rs
+++ engine/src/query/merge_strategy.rs
@@ -1,50 +1,15 @@
 use std::cmp::Ordering;
 
 use super::{PartitionState, SortOrder};
-use crate::{ErrorKind, Result};
+use crate::Result;
 
 pub enum MergeStrategy {
     Ordered(Vec<SortOrder>),
     Unordered,
 }
 
-fn compare_partitions_by_items(
-    orderings: &[SortOrder],
-    left: &PartitionState,
-    right: &PartitionState,
-) -> Result<Ordering> {
-    todo!()
-    // let (mut left_values, mut right_values) = match (left.queue.front(), right.queue.front()) {
-    //     (Some(left), Some(right)) => (left.iter_order_by_values(), right.iter_order_by_values()),
-    //     (None, Some(_)) => return Ok(Ordering::Less),
-    //     (Some(_), None) => return Ok(Ordering::Greater),
-    //     (None, None) => return Ok(Ordering::Equal),
-    // };
-    // for ordering in orderings {
-    //     let (Some(left), Some(right)) = (left_values.next(), right_values.next()) else {
-    //         return Err(ErrorKind::QueryPlanInvalid
-    //             .with_message("items have inconsistent numbers of order by items"));
-    //     };
-    //     let order = left.cmp(&right);
-    //     let order = match ordering {
-    //         SortOrder::Ascending => order,
-    //         SortOrder::Descending => order.reverse(),
-    //     };
-    //     if order != Ordering::Equal {
-    //         // The values are different, so we can return the order
-    //         return Ok(order);
-    //     }
-    // }
-
-    // // The values have been equal so far, so compare the partition keys
-    // Ok(left.pkrange.min_inclusive.cmp(&right.pkrange.min_inclusive))
-}
-
 impl MergeStrategy {
-    pub fn next_partition<'a>(
-        &self,
-        partitions: &'a mut [PartitionState],
-    ) -> Result<Option<&'a mut PartitionState>> {
+    pub fn next_item<'a>(&self, partitions: &'a mut [PartitionState]) -> ! {
         let mut next_partition = None;
         for partition in partitions {
             if partition.queue.is_empty() {
@@ -63,7 +28,7 @@ impl MergeStrategy {
             }
         }
 
-        Ok(next_partition)
+        todo!("Return batch of next items, or request for more data, or completion")
     }
 
     fn compare_partitions(
@@ -75,7 +40,20 @@ impl MergeStrategy {
             MergeStrategy::Unordered => {
                 Ok(left.pkrange.min_inclusive.cmp(&right.pkrange.min_inclusive))
             }
-            MergeStrategy::Ordered(ordering) => compare_partitions_by_items(&ordering, left, right),
+            MergeStrategy::Ordered(orderings) => {
+                let (left_item, right_item) = match (left.queue.front(), right.queue.front()) {
+                    (Some(left), Some(right)) => (left, right),
+                    (None, Some(_)) => return Ok(Ordering::Less),
+                    (Some(_), None) => return Ok(Ordering::Greater),
+                    (None, None) => return Ok(Ordering::Equal),
+                };
+                match left_item.compare(right_item, orderings)? {
+                    Ordering::Equal => {
+                        Ok(left.pkrange.min_inclusive.cmp(&right.pkrange.min_inclusive))
+                    }
+                    order => Ok(order),
+                }
+            }
         }
     }
 }
