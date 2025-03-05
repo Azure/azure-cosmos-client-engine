13adc9bbb9d0154967f3adffc8c8a1ad3f593daf
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-03-05 23:52:12 +0000

    include vendored build configs

---
+++ go/azcosmoscx/build_vendored_darwin_arm64.go
@@ -0,0 +1,7 @@
+//go:build !local && !dynamic && linux && amd64
+
+package azcosmoscx
+
+// #cgo LDFLAGS: ${SRCDIR}/libcosmoscx-vendor/aarch64-apple-darwin/libcosmoscx.a -ldl
+// #include <cosmoscx.h>
+import "C"
---
+++ go/azcosmoscx/build_vendored_windows_amd64.go
@@ -0,0 +1,7 @@
+//go:build !local && !dynamic && windows && amd64
+
+package azcosmoscx
+
+// #cgo LDFLAGS: ${SRCDIR}/libcosmoscx-vendor/x86_64-pc-windows-msvc/libcosmoscx.a -ldl
+// #include <cosmoscx.h>
+import "C"
