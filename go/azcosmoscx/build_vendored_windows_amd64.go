6e065cffe63df0dfdc053f7d1f9f1c9bd90fd553
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-07-31 20:47:59 +0000

    specify correct native libs for Go

--- go/azcosmoscx/build_vendored_windows_amd64.go
+++ go/azcosmoscx/build_vendored_windows_amd64.go
@@ -5,6 +5,6 @@
 
 package azcosmoscx
 
-// #cgo LDFLAGS: ${SRCDIR}/libcosmoscx-vendor/x86_64-pc-windows-gnu/libcosmoscx.a -lkernel32 -ladvapi32 -lntdll -luserenv -lws2_32 -ldbghelp
+// #cgo LDFLAGS: ${SRCDIR}/libcosmoscx-vendor/x86_64-pc-windows-gnu/libcosmoscx.a -lwinapi_gdi32 -lwinapi_kernel32 -lwinapi_msimg32 -lwinapi_opengl32 -lwinapi_winspool -lkernel32 -lntdll -luserenv -lws2_32 -ldbghelp
 // #include <cosmoscx.h>
 import "C"
