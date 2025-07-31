792c94da16cf95f6f27e21afc6b7155b7871620e
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-07-31 18:46:46 +0000

    progress! now debug poetry :(

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -8,12 +8,16 @@ pip install maturin
 pip install poetry
 poetry config virtualenvs.in-project true
 
-Write-Host "Installing Rust dependencies..."
-cargo install --locked cbindgen@0.29.0
+Write-Host "Env info:"
+poetry env info
+Write-Host (poetry -C ./python env activate)
 
 Write-Host "Installing Python dependencies..."
 Invoke-Expression (poetry -C ./python env activate)
 poetry -C ./python install
+
+Write-Host "Installing Rust dependencies..."
+cargo install --locked cbindgen@0.29.0
 
 Write-Host "Installing addlicense..."
 go install github.com/google/addlicense@v1.1.1
