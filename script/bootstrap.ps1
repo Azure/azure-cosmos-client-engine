1b1aeb683948da2bd09a85aab3bc6babe887e7fb
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-07-31 17:58:28 +0000

    more debugging

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -3,6 +3,17 @@ $RepoRoot = Split-Path $PSScriptRoot -Parent
 # Check for dependencies we don't automatically install
 & "$RepoRoot/script/check-deps.ps1"
 
+# Dump the path variable?
+Write-Host "Current PATH: $env:PATH"
+
+# Current Python
+$pythonPath = py -c "import sys; print(sys.executable)"
+Write-Host "Current Python: $pythonPath"
+
+# Current Pip
+$pipPath = py -c "import sys; print(sys.exec_prefix)"
+Write-Host "Current Pip: $pipPath"
+
 Write-Host "Install pipx"
 py -m pip install --user pipx
 
