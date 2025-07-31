dfa2a04a52312029c279027d68dceff16c67ad63
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-07-31 19:34:32 +0000

    add a step to check the native libraries required by the engine

--- go/azcosmoscx/build_local_windows_amd64.go
+++ go/azcosmoscx/build_local_windows_amd64.go
@@ -5,7 +5,7 @@
 
 package azcosmoscx
 
-// #cgo debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-msvc/debug/lib/cosmoscx.lib -lwinapi_gdi32 -lwinapi_kernel32 -lwinapi_msimg32 -lwinapi_opengl32 -lwinapi_winspool -lkernel32 -ladvapi32 -lntdll -luserenv -lws2_32 -ldbghelp
-// #cgo !debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-msvc/release/lib/cosmoscx.lib -lwinapi_gdi32 -lwinapi_kernel32 -lwinapi_msimg32 -lwinapi_opengl32 -lwinapi_winspool -lkernel32 -ladvapi32 -lntdll -luserenv -lws2_32 -ldbghelp
+// #cgo debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-gnu/debug/lib/cosmoscx.a
+// #cgo !debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-gnu/release/lib/cosmoscx.a
 // #include <cosmoscx.h>
 import "C"
