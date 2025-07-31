881ec9ad2ec98b24d50998106b7aacc2ef3c4977
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-07-31 18:32:20 +0000

    I give up on 'pipx

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -3,27 +3,9 @@ $RepoRoot = Split-Path $PSScriptRoot -Parent
 # Check for dependencies we don't automatically install
 & "$RepoRoot/script/check-deps.ps1"
 
-# Dump the path variable?
-Write-Host "Current PATH: $env:PATH"
-
-# Current Python
-$pythonPath = python -c "import sys; print(sys.executable)"
-Write-Host "Current Python: $pythonPath"
-
-# Current Pip
-$pipPath = python -c "import sys; print(sys.exec_prefix)"
-Write-Host "Current Pip: $pipPath"
-
-# Current Python Version
-$pythonVersion = python -c "import sys; print(sys.version)"
-Write-Host "Current Python Version: $pythonVersion"
-
-Write-Host "Install pipx"
-python -m pip install pipx
-
 Write-Host "Installing Python build tools..."
-pipx install --python $env:PY_PYTHON maturin
-pipx install --python $env:PY_PYTHON poetry
+pip install maturin
+pip install poetry
 poetry config virtualenvs.in-project true
 
 Write-Host "Installing Rust dependencies..."
