61bef3dd0e803150a09333b6014b6e85f56f3d96
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-08-06 19:58:12 +0000

    LIB cosmoscx.a

--- go/azcosmoscx/build_local_windows_amd64.go
+++ go/azcosmoscx/build_local_windows_amd64.go
@@ -5,7 +5,7 @@
 
 package azcosmoscx
 
-// #cgo debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-gnu/debug/lib/cosmoscx.a -lgdi32 -lkernel32 -lmsimg32 -lopengl32 -lwinspool -lkernel32 -lntdll -luserenv -lws2_32 -ldbghelp
-// #cgo !debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-gnu/release/lib/cosmoscx.a -lgdi32 -lkernel32 -lmsimg32 -lopengl32 -lwinspool -lkernel32 -lntdll -luserenv -lws2_32 -ldbghelp
+// #cgo debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-gnu/debug/lib/libcosmoscx.a -lgdi32 -lkernel32 -lmsimg32 -lopengl32 -lwinspool -lkernel32 -lntdll -luserenv -lws2_32 -ldbghelp
+// #cgo !debug LDFLAGS: ${SRCDIR}/../../artifacts/x86_64-pc-windows-gnu/release/lib/libcosmoscx.a -lgdi32 -lkernel32 -lmsimg32 -lopengl32 -lwinspool -lkernel32 -lntdll -luserenv -lws2_32 -ldbghelp
 // #include <cosmoscx.h>
 import "C"
